8ecefcfa82901130a3b51a739329eefc6e20eca4
TheEvilTopHat <joshuapralls@gmail.com>
2019-10-24 13:22:24 -0400

    added the groupdescriptor and inode class files

---
+++ code/ext2Reader/src/main/java/GroupDescriptor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author joshua
+ */
+
+
+//TODO EVERYTHING
+public class GroupDescriptor {
+    
+}
