24b7008a20ec24a4b10ecedc7586811f06cd015c
TheEvilTopHat <joshuapralls@gmail.com>
2019-10-24 12:59:29 -0400

    added new superblock class and outline for more code in main class

--- code/ext2Reader/src/main/java/ext2Reader.java
+++ code/ext2Reader/src/main/java/ext2Reader.java
@@ -9,28 +9,44 @@
  * @author joshua
  */
 
+//imports for file handling 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+
+//imports for text input
 import java.util.Scanner;
 
-
 public class ext2Reader {
+    
+    static void changeFolder(String folder){
+        
+    }
+    static void copyFile(String from, String to){
+        
+    }
+    static String listContents(){
+        return "";
+    }
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        //scanner creattion
+        Scanner scan = new Scanner(System.in);
+        //file system
+        RandomAccessFile raf;
         //try to open file throw exception if no file found break if found file
         while(true){
             try {
-                Scanner scan = new Scanner(System.in);
+                //on my pc it's at /home/joshua/Documents/csc400Project/virtdisk
                 String filename = scan.nextLine();
-                RandomAccessFile raf = new RandomAccessFile(filename,"r");
+                raf = new RandomAccessFile(filename,"r");
                 System.out.println("OPENING FILE");
                 break;
             }
@@ -38,6 +54,50 @@ public class ext2Reader {
                 System.out.println("NO FILE FOUND");
             }
         }
+        
+        //create superblock class and pass the file
+        Superblock sb = new Superblock(raf); //superblock class
+        //go to root
+        changeFolder("/");
+        
+        String command = "";
+        //command loop
+        while(command.equalsIgnoreCase("q") == false){
+            //get input
+            command = scan.nextLine(); 
+            //parse and do command
+            switch(command){
+                case "h":
+                case "H":
+                    System.out.println("help list");
+                    break;
+                case "cd":
+                    System.out.println("change dir");
+                    break;
+                case "ls":
+                    System.out.println("list directory contents");
+                    break;
+                case "cp":
+                    System.out.println("copy contents to host device");
+                    break;
+                case "q":
+                case "Q":
+                    System.out.println("goodbye");
+                    break;
+                default:
+                    System.out.println("Not a reconized command");
+                    break;
+            }
+        }
+        
+        //clean up area
+        try {
+            raf.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        
     }
     
 }
